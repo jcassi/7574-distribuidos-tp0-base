b8a58018f723c0f8f849fb9317a71b745d5c6d44
Julián Cassi <julcassi@gmail.com>
2024-03-18 18:32:50 -0300

    Ejercicio 1.1
    Agrego script para crear el docker compose con una cantidad configurable de clientes

---
+++ make-docker-compose.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied"
+    exit
+fi
+
+echo "version: '3.9'
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net"> temp.yaml
+
+for (( i = 1; i <= $1; i++ )) 
+do
+echo "
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server" >> temp.yaml
+done
+
+echo "
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24" >> temp.yaml
+
+mv temp.yaml docker-compose-dev.yaml
